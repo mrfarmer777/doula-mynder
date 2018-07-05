c561f6773e3fd25ba5319c8ad06149376a41dee2
Matt Farmer <mrfarmer777@gmail.com>
2018-07-05 02:27:48 +0000

    creates signup system with basic form validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,71 @@ class ApplicationController<Sinatra::Base
 
 
 
+  #////////////USER ACTIONS//////////////
+  get "/signup" do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect "/dashboard"
+    end
+  end
+
+  post "/signup" do
+    if !params[:username].empty? && !params[:password].empty?
+      @user=User.create(params)
+      @user.save
+      session[:user_id]=@user.id
+      redirect "/dashboard"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get "/login" do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect "/dashboard"
+    end
+  end
+
+  #doula dashboard view
+  get "/dashboard" do
+    if logged_in?
+      @user=current_user
+      erb :"users/dashboard"
+    else
+      redirect "/login"
+    end
+  end
+
+  #public profile view
+  get "/users/:id" do
+    if logged_in?
+      @user=User.find(params[:id])
+      erb :'users/dashboard'
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/logout" do
+    session.delete(:user_id)
+    redirect "/"
+  end
+
+
+
+  #//////////////HELPERS/////////////////
+  helpers do
+    def logged_in?
+      !!session[:user_id] #! First '!'' checks if its falsey, second one flips the boolean.
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 
 end
---
+++ spec/application_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'pry'
+
+describe ApplicationController do
+
+  describe "Homepage" do
+    it "loads the homepage" do
+      get "/"
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome to Doula Mynder")
+    end
+  end
+
+  describe "Signup Page" do
+    it "loads the signup page" do
+      get "/signup"
+      expect(last_response.status).to eq(200)
+    end
+
+    it "creates a new user and logs them in on a valid submission." do
+      params={
+        name: "Sally Ride",
+        username: "sallyride",
+        email: "sally@ride.com",
+        password: "rockets",
+        company_name: "Rocket Doula",
+      }
+
+      post "/signup", params
+      get "/signup"
+      expect(last_response.location).to include("/dashboard")
+
+    end
+
+    it "redirects to login when form submitted with empty username" do
+      user_count=User.all.count
+
+      params={
+        name: "Sally Ride",
+        username: "",
+        email: "sally@ride.com",
+        password: "rockets",
+        company_name: "Rocket Doula",
+      }
+
+      post "/signup", params
+      new_user_count=User.all.count
+      expect(last_response.location).to include("/signup")
+      expect(new_user_count).to eq(user_count)
+    end
+
+  end
+end
