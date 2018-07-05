e7332303cfe660ba7fc047fb1a322c5aedb04587
Matt Farmer <mrfarmer777@gmail.com>
2018-07-05 23:55:14 +0000

    fixes navbar alignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require 'rack-flash'
 class ApplicationController<Sinatra::Base
 
   configure do
-    set :public_folder, 'public' #unused currently
+    set :public_folder, 'public'
     set :views, "app/views"
     enable :sessions
     set :session_secret, "gigijuju"
